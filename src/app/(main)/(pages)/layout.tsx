d2b71d939a4e4a04c3e4e59e79368d197cd1e1ac
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-05 07:07:05 +0530

    Refactor main layout with updated styling and simplified component

--- src/app/(main)/(pages)/layout.tsx
+++ src/app/(main)/(pages)/layout.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-type Props = {
-  children: React.ReactNode;
-};
+type Props = { children: React.ReactNode };
 
-const Layout = (props: Props) => {
+const Layout = ({ children }: Props) => {
   return (
-    <div className="flex overflow-hidden h-screen">
-      <div className="w-full">{props.children}</div>
+    <div className="border-l-[1px] border-t-[1px] pb-20 h-screen rounded-l-3xl border-muted-foreground/20 overflow-scroll ">
+      {children}
     </div>
   );
 };
