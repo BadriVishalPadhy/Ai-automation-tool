453ac9621d4b4b3f5e3ba1d642222f264a45e0c0
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-09 02:34:59 +0530

    Add enhanced form components and styling for settings page

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,3 +1,6 @@
+import { Input } from "@/components/forms";
+import { LabelInputContainer } from "@/components/label";
+import { Label } from "@radix-ui/react-dropdown-menu";
 import React from "react";
 
 type Props = {};
@@ -14,6 +17,32 @@ const Settings = (props: Props) => {
           <p className="text-base text-white/50">
             Add or update your information
           </p>
+        </div>
+        <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+          <form className="my-8">
+            <LabelInputContainer>
+              <label htmlFor="Name">First name</label>
+              <Input id="Name" placeholder="Garp" type="text" />
+            </LabelInputContainer>
+
+            <LabelInputContainer className="mb-4">
+              <label htmlFor="email">Email Address</label>
+              <Input
+                id="email"
+                placeholder="projectmayhem@fc.com"
+                type="email"
+              />
+            </LabelInputContainer>
+
+            <LabelInputContainer className="mb-8"></LabelInputContainer>
+            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+            <button
+              className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+              type="submit"
+            >
+              Sign up &rarr;
+            </button>
+          </form>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+const {
+	default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -9,6 +14,9 @@ export default {
 	],
 	theme: {
 		extend: {
+			boxShadow: {
+				input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+			},
 			fontFamily: {
 				'press-start': ['"Press Start 2P"'],
 			},
@@ -73,10 +81,25 @@ export default {
 				}
 			}
 			,
-		}
+
+		},
+
 	},
 
 
 
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"), addVariablesForColors],
+
 } satisfies Config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
