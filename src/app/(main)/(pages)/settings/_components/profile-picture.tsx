81b0c2d191a8c7e8834e70556e17bc8a98bdcd51
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-13 09:20:28 +0530

    Add UploadCare button to profile picture component

--- src/app/(main)/(pages)/settings/_components/profile-picture.tsx
+++ src/app/(main)/(pages)/settings/_components/profile-picture.tsx
@@ -6,7 +6,9 @@ const ProfilePicture = (props: Props) => {
   return (
     <div className="flex flex-col">
       <p className="text-lg text-white "> Profile Picture </p>
-      <div className="flex h-[30vh] flex-col justify-center  ">aloo</div>
+      <div className="flex h-[30vh] flex-col justify-center  ">
+        <UploadCareButton />
+      </div>
     </div>
   );
 };
