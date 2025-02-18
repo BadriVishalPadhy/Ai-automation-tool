48bcc89a4f80a779b9570b59a5f9e34b2339dbce
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-18 06:23:44 +0530

    Refactor UploadCare button with updated import and upload handling

--- src/app/(main)/(pages)/settings/_components/upload-care-button.tsx
+++ src/app/(main)/(pages)/settings/_components/upload-care-button.tsx
@@ -3,29 +3,38 @@ import { FileUploaderRegular } from "@uploadcare/react-uploader/next";
 import "@uploadcare/react-uploader/core.css";
 import React, { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
-import * as LR from "@uploadcare/react-uploader";
+import * as LR from "@uploadcare/file-uploader";
 type Props = {
   onUpload: any;
 };
 
+LR.ActivityBlock;
 const UploadCareButton = ({ onUpload }: Props) => {
   const router = useRouter();
-  const ctxProvider = useRef<
+  const ctxProviderRef = useRef<
     typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
   >(null);
   useEffect(() => {
     const handleUpload = async (e: any) => {
-      const file = await onUpload;
+      const file = await onUpload(e.detail.cdnUrl);
+      if (file) {
+        router.refresh();
+      }
     };
+    ctxProviderRef.current?.addEventListener(
+      "file-upload-success",
+      handleUpload
+    );
   }, []);
   return (
     <div>
-      <FileUploaderRegular
+      UploadCAreButton
+      {/* <FileUploaderRegular
         sourceList="local, camera, facebook, gdrive"
         cameraModes="photo, video"
         classNameUploader="uc-light"
         pubkey="a096ea9ff6e42963cc61"
-      />
+      /> */}
     </div>
   );
 };
