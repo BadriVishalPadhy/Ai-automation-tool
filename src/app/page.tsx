cafeac85c25ce2bce708fb6ea8d66145a8c82767
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-21 19:05:06 +0530

    Add letter glitch effect component with OGL library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import { Navbar } from "@/components/global/navbar";
 import Image from "next/image";
+import LetterGlitch from "@/components/global/landing-page";
+import { string } from "zod";
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
+      <div>
+        <LetterGlitch
+          glitchSpeed={50}
+          centerVignette={true}
+          outerVignette={false}
+          smooth={true}
+          glitchColors={["#2b4539", "#61dca3", "#61b3dc"]}
+        />
+        <Navbar />
+      </div>
     </main>
   );
 }
