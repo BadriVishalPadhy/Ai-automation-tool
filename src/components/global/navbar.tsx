e724f83ac94f4e93913cb520e820563ea7b533f4
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-22 19:09:00 +0530

    Add layout, constants, types, and utils for dashboard navigation and workflow management

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -49,7 +49,8 @@ export function Navbar() {
           <div className="hidden md:block">
             <div className="ml-4 flex items-center md:ml-6">
               <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                Shimmer
+                Dashboard
+                <Link href={"/Dashboard"}></Link>
               </button>
               <ThemeToggle />
               <UserMenu />
---
+++ src/providers/lib/constants.tsx
@@ -0,0 +1,17 @@
+import Category from "@/components/icons/category";
+import Logs from "@/components/icons/clipboard";
+import Templates from "@/components/icons/cloud_download";
+import Home from "@/components/icons/home";
+import Payment from "@/components/icons/payment";
+import Settings from "@/components/icons/settings";
+import Workflows from "@/components/icons/workflows";
+// import { Connection } from './types'
+export const menuOptions = [
+  { id: 1, name: "Dashboard", Component: Home, href: "/dashboard" },
+  { id: 2, name: "Workflows", Component: Workflows, href: "/workflows" },
+  { id: 3, name: "Settings", Component: Settings, href: "/settings" },
+  { id: 4, name: "Connections", Component: Category, href: "/connections" },
+  { id: 5, name: "Billing", Component: Payment, href: "/billing" },
+  { id: 6, name: "Templates", Component: Templates, href: "/templates" },
+  { id: 7, name: "Logs", Component: Logs, href: "/logs" },
+];
