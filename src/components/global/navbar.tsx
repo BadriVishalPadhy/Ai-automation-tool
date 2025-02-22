3325792b095ed263a099bf8c6f72582584cb0d51
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-22 19:23:52 +0530

    Fix Dashboard button navigation in Navbar component

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -48,10 +48,11 @@ export function Navbar() {
           </div>
           <div className="hidden md:block">
             <div className="ml-4 flex items-center md:ml-6">
-              <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-                Dashboard
-                <Link href={"/Dashboard"}></Link>
-              </button>
+              <Link href="/dashboard">
+                <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                  Dashboard
+                </button>{" "}
+              </Link>
               <ThemeToggle />
               <UserMenu />
             </div>
