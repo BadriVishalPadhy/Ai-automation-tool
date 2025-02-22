c7b277aa00a9f6eebcf56d6a7c5cee6ac9aca431
Badri-Vishal <badrivishalpadhy@gmail.com>
2025-02-22 18:54:27 +0530

    Refactor Clerk authentication routes and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/api/clerk-webhook',
-  '/api/drive-activity/notification',
-  '/api/payment/success'
-])
+// const isPublicRoute = createRouteMatcher([
+//   '/',
+//   '/api/clerk-webhook',
+//   '/api/drive-activity/notification',
+//   '/api/payment/success'
+// ])
 
-const isIgnoredRoute = createRouteMatcher([
-  '/api/auth/callback/discord',
-  '/api/auth/callback/notion',
-  '/api/auth/callback/slack',
-  '/api/flow',
-  '/api/cron/wait'
-])
+// const isIgnoredRoute = createRouteMatcher([
+//   '/api/auth/callback/discord',
+//   '/api/auth/callback/notion',
+//   '/api/auth/callback/slack',
+//   '/api/flow',
+//   '/api/cron/wait'
+// ])
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req) && !isIgnoredRoute(req)) {
-    await auth.protect()
-  }
-})
+// export default clerkMiddleware(async (auth, req) => {
+//   if (!isPublicRoute(req) && !isIgnoredRoute(req)) {
+//     await auth.protect()
+//   }
+// })
+export default clerkMiddleware()
+
 
 export const config = {
   matcher: [
